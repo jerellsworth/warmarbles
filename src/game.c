f0a89ba2072fb39ee8deba0e9f4dbad75400d290
Zachary Rubenstein <zacharyrubenstein@Zacharys-MacBook-Pro.local>
2025-01-01 13:08:09 -0600

    fixed win screen palette

--- src/game.c
+++ src/game.c
@@ -5,7 +5,7 @@ Game *Game_init(u8 n_players, SFX *sfx) {
     g->n_players = n_players;
     g->sfx = sfx;
     SPR_init();
-    PAL_setPalette(PAL0, PAL_TITLE_1.data, DMA);
+    PAL_setPalette(PAL0, PAL_BOARD.data, DMA);
     PAL_setPalette(PAL1, PAL_MARBLE.data, DMA);
     PAL_setPalette(PAL2, PAL_GUY.data, DMA);
     PAL_setPalette(PAL3, PAL_TARGET.data, DMA);
